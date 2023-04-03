4b570c3312a68cb1c96ad571fe56231d6e1a79ce
Jeff <Jeff@LAPTOP-53DFDE7P.lan>
2023-04-03 12:54:42 -0500

    Punch list to Json finished

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
@@ -5,6 +5,7 @@ import java.util.*;
 import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 import com.github.cliftonlabs.json_simple.*;
+import edu.jsu.mcis.cs310.tas_sp23.Punch;
 
 /**
  * 
@@ -14,5 +15,30 @@ import com.github.cliftonlabs.json_simple.*;
  * 
  */
 public final class DAOUtility {
+
+    public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist) {
+        
+        JsonArray jsonArr = new JsonArray();
+        
+        for (int x = 0; x < dailypunchlist.size(); x++){
+            
+            Punch punch = dailypunchlist.get(x);
+            
+            JsonObject obj = new JsonObject();
+
+            obj.put("id", String.valueOf(punch.getId()));
+            obj.put("badgeid", String.valueOf(punch.getBadgeid()));
+            obj.put("punchtype", String.valueOf(punch.getPunchtype()));
+            obj.put("adjustmenttype", String.valueOf(punch.getAdjustmenttype()));
+            obj.put("originaltimestamp", String.valueOf(punch.getOriginaltimestamp()));
+            obj.put("adjustedtimestamp", String.valueOf(punch.getAdjustedtimestamp()));
+            
+            jsonArr.add(obj);
+        }
+
+        String json = Jsoner.serialize(jsonArr);
+        
+        return json;
+    }
 
 }
