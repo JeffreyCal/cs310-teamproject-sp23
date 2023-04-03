48d66e21d44d36464cdddb7501a47511e1a12979
JeffreyCal <jeffreypatterson02@hotmail.com>
2023-04-03 00:09:44 -0500

    Stuck with weird error message when testing punch list to json, otherwise works fine

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
@@ -5,6 +5,10 @@ import java.util.*;
 import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 import com.github.cliftonlabs.json_simple.*;
+import edu.jsu.mcis.cs310.tas_sp23.Badge;
+import edu.jsu.mcis.cs310.tas_sp23.EventType;
+import edu.jsu.mcis.cs310.tas_sp23.Punch;
+import edu.jsu.mcis.cs310.tas_sp23.PunchAdjustmentType;
 
 /**
  * 
@@ -15,4 +19,35 @@ import com.github.cliftonlabs.json_simple.*;
  */
 public final class DAOUtility {
 
+    public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist){
+        
+        JsonArray jsonData = new JsonArray();  
+        JsonObject jsonObject = new JsonObject();
+        
+        for (int x = 0; x < dailypunchlist.size(); x++){
+            
+            Punch punch = dailypunchlist.get(x);
+            
+            jsonObject.put("id", String.valueOf(punch.getId()));
+            
+            jsonObject.put("terminalid", String.valueOf(punch.getTerminalid()));
+            
+            jsonObject.put("badgeid", String.valueOf(punch.getBadgeid()));
+
+            jsonObject.put("punchtype", String.valueOf(punch.getPunchtype()));
+            
+            jsonObject.put("originaltimestamp", String.valueOf(punch.getOriginaltimestamp()));
+            
+            jsonObject.put("adjustmenttype", String.valueOf(punch.getAdjustmenttype()));
+            
+            jsonObject.put("adjustedtimestamp", String.valueOf(punch.getAdjustedtimestamp()));
+            
+            jsonData.add(jsonObject);
+
+        }
+        
+        String json = Jsoner.serialize(jsonData);
+        
+        return json;
+    }
 }
