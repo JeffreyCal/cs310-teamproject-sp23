e9a3f4e9ebd5a71b0ae95490d0e384dc98522a90
JeffreyCal <jeffreypatterson02@hotmail.com>
2023-04-17 01:35:40 -0500

    Almost done with adjust, problem with formatting adjustment type and on weekend shifts

--- src/edu/jsu/mcis/cs310/tas_sp23/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Punch.java
@@ -35,7 +35,7 @@ public class Punch {
         this.terminalid = terminalid;
         this.badge = badge;
         this.badgeid = badge.getId();
-        this.originaltimestamp = LocalDateTime.now();
+        this.originaltimestamp = originaltimestamp;
         this.timestamp = originaltimestamp;
         this.punchtype = punchtype;
     }
@@ -121,25 +121,109 @@ public class Punch {
         return s.toString().toUpperCase();
 
     }
-
     public void adjust(Shift s) {
-        if(originaltimestamp.getDayOfWeek() == DayOfWeek.SATURDAY || originaltimestamp.getDayOfWeek() == DayOfWeek.SUNDAY ){
-           adjustmenttype = PunchAdjustmentType.INTERVAL_ROUND;
-           adjustedlunchstatus = LunchStatus.INAPPLICABLE;
-        }
-        else {
-            switch(punchtype){
+
+        int shiftStart = s.getShiftstart().toSecondOfDay();
+        int shiftStop = s.getShiftstop().toSecondOfDay();
+        int lunchStart = s.getLunchstart().toSecondOfDay();
+        int lunchStop = s.getLunchstop().toSecondOfDay();
+        int gracePeriod = s.getGraceperiod() * 60;
+        int dockPen = s.getDockpenalty() * 60;
+        int roundInt = s.getRoundinterval() * 60;
+        int originalTimeSec = originaltimestamp.toLocalTime().toSecondOfDay();
+
+        if (originaltimestamp.getDayOfWeek() == DayOfWeek.SATURDAY || originaltimestamp.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            adjustmenttype = PunchAdjustmentType.INTERVAL_ROUND;
+            int remainderSec = originalTimeSec % roundInt;
+            if (remainderSec < roundInt / 2) {
+                int roundedTime = originalTimeSec - remainderSec;
+                adjustedtimestamp = LocalTime.ofSecondOfDay(roundedTime).atDate(originaltimestamp.toLocalDate());
+            } else {
+                int roundedTime = originalTimeSec + remainderSec;
+                adjustedtimestamp = LocalTime.ofSecondOfDay(roundedTime).atDate(originaltimestamp.toLocalDate());
+            }
+            adjustedlunchstatus = LunchStatus.INAPPLICABLE;
+        } else {
+            switch (punchtype) {
                 case CLOCK_IN:
-                    //if they clock in after lunch, set adjustmenttype to lunchstop
-                    //else if clock in time is after shiftstart
-                        //if clocking in between the start of their shift and the start of shift + the grace period, set adjustment type to shiftstart
-                        //else adjustmenttype = dock
-                    //else adjustment type round
+
+                    if (originalTimeSec < shiftStart) {
+                        if (roundInt >= shiftStart - originalTimeSec) {
+                            adjustmenttype = PunchAdjustmentType.SHIFT_START;
+                            adjustedtimestamp = LocalTime.ofSecondOfDay(shiftStart).atDate(originaltimestamp.toLocalDate());
+                        } else if (roundInt < shiftStart - originalTimeSec) {
+                            adjustmenttype = PunchAdjustmentType.INTERVAL_ROUND;
+                            int remainderSec = originalTimeSec % roundInt;
+                            if (remainderSec < roundInt / 2) {
+                                int roundedTime = originalTimeSec - remainderSec;
+                                adjustedtimestamp = LocalTime.ofSecondOfDay(roundedTime).atDate(originaltimestamp.toLocalDate());
+                            } else {
+                                int roundedTime = originalTimeSec + remainderSec;
+                                adjustedtimestamp = LocalTime.ofSecondOfDay(roundedTime).atDate(originaltimestamp.toLocalDate());
+                            }
+                            
+                        } else {
+                            adjustmenttype = PunchAdjustmentType.NONE;
+                            adjustedtimestamp = originaltimestamp;
+                        }
+                    } else if (originalTimeSec < lunchStart) {
+                        if (shiftStart + gracePeriod < originalTimeSec) {
+                            adjustmenttype = PunchAdjustmentType.SHIFT_DOCK;
+                            int dock = shiftStart + dockPen;
+                            adjustedtimestamp = LocalTime.ofSecondOfDay(dock).atDate(originaltimestamp.toLocalDate());
+                        } else {
+                            adjustmenttype = PunchAdjustmentType.SHIFT_START;
+                            adjustedtimestamp = LocalTime.ofSecondOfDay(shiftStart).atDate(originaltimestamp.toLocalDate());
+                        }
+                    } else if (originalTimeSec > lunchStart) {
+                        adjustmenttype = PunchAdjustmentType.LUNCH_STOP;
+                        adjustedtimestamp = LocalTime.ofSecondOfDay(lunchStop).atDate(originaltimestamp.toLocalDate());
+                    }
                     break;
                 case CLOCK_OUT:
+                    if (originalTimeSec > shiftStop) {
+                        if (originalTimeSec - shiftStop > roundInt) {
+                            adjustmenttype = PunchAdjustmentType.INTERVAL_ROUND;
+                            int remainderSec = originalTimeSec % roundInt;
+                            if (remainderSec < roundInt / 2) {
+                                int roundedTime = originalTimeSec - remainderSec;
+                                adjustedtimestamp = LocalTime.ofSecondOfDay(roundedTime).atDate(originaltimestamp.toLocalDate());
+                            } else {
+                                int roundedTime = originalTimeSec + remainderSec;
+                                adjustedtimestamp = LocalTime.ofSecondOfDay(roundedTime).atDate(originaltimestamp.toLocalDate());
+                            }
+                        } else {
+                            adjustmenttype = PunchAdjustmentType.SHIFT_STOP;
+                            adjustedtimestamp = LocalTime.ofSecondOfDay(shiftStop).atDate(originaltimestamp.toLocalDate());
+                        }
+                    } else if (originalTimeSec > lunchStart && originalTimeSec < lunchStop) {
+                        adjustmenttype = PunchAdjustmentType.LUNCH_START;
+                        adjustedtimestamp = LocalTime.ofSecondOfDay(lunchStart).atDate(originaltimestamp.toLocalDate());
+                    } else if (originalTimeSec < shiftStop) {
+                        if (shiftStop - gracePeriod <= originalTimeSec) {
+                            adjustmenttype = PunchAdjustmentType.SHIFT_STOP;
+                            adjustedtimestamp = LocalTime.ofSecondOfDay(shiftStop).atDate(originaltimestamp.toLocalDate());
+                        }else {
+                            adjustmenttype = PunchAdjustmentType.SHIFT_DOCK;
+                            int dock = shiftStart - dockPen;
+                            adjustedtimestamp = LocalTime.ofSecondOfDay(dock).atDate(originaltimestamp.toLocalDate());
+                        }
+                    }
                     break;
             }
         }
     }
+    
+    public String printAdjusted() {
+        
+        StringBuilder s = new StringBuilder();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MM/dd/yyyy HH:mm:ss");
+        String formatted = adjustedtimestamp.format(formatter);
+        String atype = adjustmenttype.toString();
+       
+        s.append("#").append(badgeid).append(" ").append(punchtype).append(": ").append(formatted).append(" (").append(atype).append(")");
+        
+        return s.toString().toUpperCase();
 
+    }
 }
